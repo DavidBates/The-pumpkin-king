4ccaedb2857b7f737fcb5951dadffa5f6ae74b43
David Bates <github@dlbates.com>
2023-10-07 16:53:14 -0400

    Initial Commit

---
+++ pumpking/pumpkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  pumpkingApp.swift
+//  pumpking
+//
+//  Created by David Bates on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct pumpkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
